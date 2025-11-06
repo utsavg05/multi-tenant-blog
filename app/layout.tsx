7794de1e314be4a12d2cacaa1038376961ea567c
utsavg05 <utsav.gp1204@gmail.com>
2025-11-06 21:58:25 +0530

    Clerk setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {ClerkProvider, SignedIn, SignedOut, SignIn} from '@clerk/nextjs'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClerkProvider>
+          <SignedOut>
+            <div className="min-h-screen min-w-screen flex justify-center items-center">
+            <SignIn routing="hash"/>
+            </div>
+          </SignedOut>
+          <SignedIn>{children}</SignedIn>
+        
+        </ClerkProvider>
       </body>
     </html>
   );
